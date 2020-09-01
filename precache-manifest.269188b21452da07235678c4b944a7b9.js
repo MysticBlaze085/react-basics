ef77d611747888af88170919d6adbef137313e54
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-09-01 18:48:01 -0500

    Updates

---
+++ precache-manifest.269188b21452da07235678c4b944a7b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf591b9143b32daab3bae97111e7c32",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "1fe2610475318d56b8d3",
+    "url": "/react-basics/static/css/main.377763ba.chunk.css"
+  },
+  {
+    "revision": "2aa89dde0859bdbbf2dc",
+    "url": "/react-basics/static/js/2.c3b51709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics/static/js/2.c3b51709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe2610475318d56b8d3",
+    "url": "/react-basics/static/js/main.06bc6334.chunk.js"
+  },
+  {
+    "revision": "ecb067e5745a49c636d5",
+    "url": "/react-basics/static/js/runtime-main.74e35950.js"
+  }
+]);
